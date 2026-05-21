b14ecc8ccc637b9e45f34b6ccc61ed66daabd33c
KylanGillam <kylan@founditmarketing.com>
2026-05-21 11:28:13 -0500

    Remove jason@founditmarketing.com from email recipients

--- api/contact.ts
+++ api/contact.ts
@@ -23,8 +23,8 @@ export default async function handler(req: any, res: any) {
     insightEmail = 'robbie@insighthelps.com';
   }
 
-  // All submissions go to jason and the designated insight email
-  const toEmail = ['jason@founditmarketing.com', insightEmail];
+  // Submissions go to the designated insight email
+  const toEmail = [insightEmail];
 
   try {
     const { data, error } = await resend.emails.send({
